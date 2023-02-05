e750e26ddd071708cf72654c3a2d3875c3f3f605
mhunkalo <dr.hunkalo@gmail.com>
2023-02-05 22:22:59 +0100

    task_1

--- homework_3/task_1.py
+++ homework_3/task_1.py
@@ -1,8 +1,20 @@
+#a
 def interchange_values(a, b):
     c = a
     a = b
     b = c
     return (a, b)
 print(interchange_values(2, 3))
+#b
+a = 2
+b = 3
 a, b  = b, a
+print(a,b)
+#c
+a = 2
+b = 3
+a = a + b
+b = a - b
+a = a - b
+print(a,b)
 
