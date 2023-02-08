89b9f5cf9805c95625c2b94db254b308ff67d018
mhunkalo <dr.hunkalo@gmail.com>
2023-02-08 21:34:06 +0100

    task_1

---
+++ homework_4/task_3.py
@@ -0,0 +1,7 @@
+a = input('Please enter an integer number:')
+
+b = int(a) + 1
+c = int(a) - 1
+
+print('Next number for number ', a, ' is ', b, '.', '\n',
+      'Previous number for number ', a, ' is ', c, '.', sep='')
