fe11cee72d15c92893398f24c430b97885699df0
mhunkalo <dr.hunkalo@gmail.com>
2023-02-19 22:57:47 +0100

    task_1

---
+++ homework_6/task_7.py
@@ -0,0 +1,19 @@
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def fibonacci_other(n, digit=0, pre_digit=1):
+    if n == 1:
+        return digit
+    return fibonacci_other(n - 1, digit=digit + pre_digit, pre_digit=digit)
+
+
+def main():
+    print(fibonacci(7))
+    print(fibonacci_other(17))
+
+
+if __name__ == '__main__':
+    main()
