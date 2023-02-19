bf5826133780b3520b630ef59b997c74271e19f7
mhunkalo <dr.hunkalo@gmail.com>
2023-02-19 22:45:48 +0100

    task_1

---
+++ homework_6/5_task.py
@@ -0,0 +1,14 @@
+def is_year_high(year):
+    return year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+
+
+def main():
+    year = int(input('Input year'))
+    if is_year_high(year):
+        print('yes')
+    else:
+        print('no')
+
+
+if __name__ == '__main__':
+    main()
